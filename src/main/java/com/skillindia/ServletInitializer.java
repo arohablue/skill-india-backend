58507c740566fe265005f6e5c2aba7f27f7e140c
arohablue <amit42308@gmail.com>
2019-02-09 10:07:30 +0530

    intial project setup

---
+++ src/main/java/com/skillindia/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.skillindia;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SkillIndiaBackendApplication.class);
+	}
+
+}
+
---
+++ src/main/java/com/skillindia/SkillIndiaBackendApplication.java
@@ -0,0 +1,14 @@
+package com.skillindia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillIndiaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillIndiaBackendApplication.class, args);
+	}
+
+}
+
